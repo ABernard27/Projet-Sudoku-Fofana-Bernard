8a59b5ad347e1a9ffb64e9591d0e73026c109bee
ABernard27 <anne.bernard@etu.umontpellier.fr>
2022-02-17 20:12:10 +0100

    existe ligne et colonne

---
+++ Sudoku/R/existeSurLigne.R
@@ -0,0 +1,17 @@
+#' Détermine s'il existe déjà le numéro sur la ligne
+#'
+#' @param X,i,k
+#' @return True ou false
+#'
+
+
+existeSurLigne <- function(X,i,k){
+  if (length(unique(X[k,]==i))==2){
+    print('TRUE')
+  }
+  else{
+    print('FALSE')
+  }
+}
+
+
