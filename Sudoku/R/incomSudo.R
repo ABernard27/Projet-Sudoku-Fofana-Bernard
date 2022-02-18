a67ca48f6b520e441ef3412757930c58d9c54fe2
ABernard27 <anne.bernard@etu.umontpellier.fr>
2022-02-18 16:37:24 +0100

    affichage, case vide

---
+++ Sudoku/R/incomSudo.R
@@ -0,0 +1,12 @@
+#' Affiche la grille de sudoku
+#'
+#' @param X
+#' @return la grille
+#'
+
+incomSudo <- function(X){
+  for (i in 1:75){
+    X[floor(runif(1,1,10)),floor(runif(1,1,10))] <- "   "
+  }
+  sudoplot(X)
+}
---
+++ Sudoku/R/sudoplot.R
@@ -0,0 +1,10 @@
+#' Affiche la grille de sudoku
+#'
+#' @param X
+#' @return la grille
+#'
+
+sudoplot <- function(X){
+  grid.table(X)
+}
+
