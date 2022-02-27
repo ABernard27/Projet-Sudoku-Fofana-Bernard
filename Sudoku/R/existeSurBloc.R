eea6e8b437aedfdee8a09a8c279b2f1f28fe6d37
sikoufof <91316018+sikoufof@users.noreply.github.com>
2022-02-27 13:30:24 +0100

    Update existeSurBloc.R

--- Sudoku/R/existeSurBloc.R
+++ Sudoku/R/existeSurBloc.R
@@ -9,10 +9,10 @@
 existeSurBloc <- function(X,i,l,c){
   bloc <- WhichBloc(X,l,c)
   if (length(unique(bloc[1,]==i))==2 | length(unique(bloc[2,]==i))==2 | length(unique(bloc[3,]==i))==2){
-    print('TRUE')
+    return(TRUE)
   }
   else{
-    print('FALSE')
+    return(FALSE)
   }
 }
 
